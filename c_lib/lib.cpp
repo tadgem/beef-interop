2072fb7ead0d721ee94ecb1b8054eda1b316c079
tadgem <98434757+tadgem@users.noreply.github.com>
2024-07-20 20:42:20 +0100

    start stubbing instances

--- c_lib/lib.cpp
+++ c_lib/lib.cpp
@@ -1,5 +1,9 @@
 #include "lib.h"
 
+void AddComponentData(foreign_component_type_data* component_data)
+{
+}
+
 void PrintHello()
 {
     std::cout << "Hello from C++!" << std::endl; 
--- c_lib/lib.h
+++ c_lib/lib.h
@@ -2,5 +2,32 @@
 
 
 extern "C" {
+
+    typedef struct
+    {
+        void* (*    type_create)();
+        void(*      type_free)(void*);
+        const char* type_name;
+        size_t      type_size;
+        bool        has_update;
+        bool        has_init;
+        bool        has_cleanup;
+
+        // foreign callbacks that pass in the created instance in C
+        void (*     component_init)(void*)       = nullptr;
+        void (*     component_update)(void*)     = nullptr;
+        void (*     component_cleanup)(void*)    = nullptr;
+    }   foreign_component_type_data;
+
+    typedef struct
+    {
+        void*       type_instance;
+        const char* type_name;
+        size_t      type_size;
+    }   foreign_component_type_instance;
+
+    
+    extern void AddComponentData(foreign_component_type_data* component_data);
+
     extern void PrintHello();
 }
